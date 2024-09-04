b8a1efbeacdb182aa7f0d5d80a89109fa2bc7d40
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-05 04:30:29 +0800

    - update main.go

--- main.go
+++ main.go
@@ -1,182 +1,241 @@
 package main
 
 import (
-	"bufio"
-	"context"
-	"flag"
-	"fmt"
-	"io/fs"
-	"log"
+	"bytes"
+	"image"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/cdproto/runtime"
-	"github.com/chromedp/chromedp"
-	"github.com/graniticio/inifile"
+	"testing"
 )
 
 var (
-	dir string
+	testDataPath      = "./test_data/"
+	testJpegImagePath = testDataPath + "test_image.jpg"
+	testPngImagePath  = testDataPath + "test_image.png"
 )
 
-func main() {
-	flag.StringVar(&dir, "dir", "", "Absolute path for target directory")
-
-	flag.Parse()
-	if len(dir) < 1 {
-		log.Fatal("No --dir is given")
-	}
-
-	urlFiles := Scan(dir, ".url")
-	urlFilesLen := len(urlFiles)
-	if urlFilesLen < 1 {
-		log.Fatal("No .url file found")
-	}
-	fmt.Printf("There are %d url files\n", len(urlFiles))
-
-	file, err := os.Create(fmt.Sprintf("%s.txt", getFolderName(dir)))
-	errExit(err)
-	defer file.Close()
-	w := bufio.NewWriter(file)
-
-	for _, s := range urlFiles {
-		ic, err := inifile.NewIniConfigFromPath(s)
-		errExit(err)
-		url, err := ic.Value("InternetShortcut", "URL")
-		errExit(err)
-		fmt.Println("checking", url, ", in", s, "...")
-		protocol := url[0:strings.Index(url, `://`)]
-		if protocol == `http` || protocol == `https` {
-			title, err := getTitle(url)
-			errExit(err)
-			fmt.Fprintf(w, "- [%s](%s)\n", title, url)
-		} else {
-			fmt.Fprintf(w, "- [%s](%s)\n", url, url)
-		}
-	}
-	errExit(w.Flush())
-
-	// for _, s := range urlFiles {
-	// 	errExit(os.Remove(s))
-	// }
+var thumbTests = []struct {
+	mimeType string
+}{
+	{"image/jpeg"},
+	{"image/png"},
 }
 
-func errExit(err error) {
-	if err != nil {
-		log.Fatal(err)
+// TestThumbTests tests thumbTests
+func TestThumbTests(t *testing.T) {
+	config := Generator{
+		DestinationPath:   "",
+		DestinationPrefix: "thumb_",
+		Scaler:            "CatmullRom",
 	}
-}
-
-func Scan(root, ext string) []string {
-	var a []string
-	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
-		if e != nil {
-			return e
-		}
-		if filepath.Ext(d.Name()) == ext {
-			a = append(a, s)
-		}
-		return nil
-	})
-	return a
-}
-
-func getTitle(urlstr string) (string, error) {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-	var title string
-
-	chromedp.ListenTarget(ctx, func(ev interface{}) {
-
-		switch ev := ev.(type) {
-
-		case *network.EventResponseReceived:
-			resp := ev.Response
-			if resp.URL == urlstr {
-				log.Printf("received headers: %s %s", resp.URL, resp.MimeType)
-				if resp.MimeType != "text/html" {
-					chromedp.Cancel(ctx)
-				}
-
-				if strings.Contains(resp.URL, "youtube.com") {
-					log.Printf("YT!!")
-				}
-
-				// may be redirected
-				switch ContentType := resp.Headers["Content-Type"].(type) {
-				case string:
-					// here v has type T
-					if !strings.Contains(ContentType, "text/html") {
-						chromedp.Cancel(ctx)
-					}
-				}
-
-				switch ContentType := resp.Headers["content-type"].(type) {
-				case string:
-					// here v has type T
-					if !strings.Contains(ContentType, "text/html") {
-						chromedp.Cancel(ctx)
-					}
-				}
+	var testImagePath string
+	for _, tt := range thumbTests {
+		t.Run(tt.mimeType, func(t *testing.T) {
+			switch mimeType := tt.mimeType; mimeType {
+			case "image/jpeg":
+				testImagePath = testJpegImagePath
+			case "image/png":
+				testImagePath = testPngImagePath
 			}
-		}
-	})
+			gen := NewGenerator(config)
 
-	req := `
-(async () => new Promise((resolve, reject) => {
-	var handle = NaN;
+			i, err := gen.NewImageFromFile(testImagePath)
+			if err != nil {
+				t.Error(err)
+			}
 
-	(function animate() {
-		if (!isNaN(handle)) {
-			clearTimeout(handle);
-		}
+			teardownTestCase := setupTestCase(t)
+			dest := testDataPath + gen.DestinationPrefix + filepath.Base(i.Path)
+			defer teardownTestCase(t, dest)
 
-		if (document.title.length > 0 && !document.title.startsWith("http")) {
-			resolve(document.title);
-		} else {
-			handle = setTimeout(animate, 1000);
-		}
-	}());
-}));
-`
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(urlstr),
-		//chromedp.Evaluate(`window.location.href`, &res),
-		chromedp.Evaluate(req, nil, func(p *runtime.EvaluateParams) *runtime.EvaluateParams {
-			return p.WithAwaitPromise(true)
-		}),
-		chromedp.Title(&title),
-	)
-	if err == context.Canceled {
-		// url as title
-		log.Printf("Cancel!!")
-		return urlstr, nil
+			thumbBytes, err := gen.CreateThumbnail(i)
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = os.WriteFile(dest, thumbBytes, 0644)
+			if err != nil {
+				t.Error(err)
+			}
+
+			checkFileExists(t, dest)
+			var (
+				wantWidth  = gen.Width
+				wantHeight = gen.Height
+			)
+			gotWidth, gotHeight, err := checkImageDimensions(dest)
+			if err != nil {
+				t.Error(err)
+			}
+			if wantWidth != gotWidth {
+				t.Errorf("checkImageDimensions() got %d, wants %d", gotWidth, wantWidth)
+			}
+			if wantHeight != gotHeight {
+				t.Errorf("checkImageDimensions() got %d, wants %d", gotHeight, wantHeight)
+			}
+		})
 	}
-
-	return title, err
 }
 
-// fmt.Println(getFolderName(`P`))           //P
-// fmt.Println(getFolderName(`P:`))          //P
-// fmt.Println(getFolderName(`P:\`))         //P
-// fmt.Println(getFolderName(`P:\testing`))  //testing
-// fmt.Println(getFolderName(`P:\testing\`)) //testing
-func getFolderName(input string) string {
-	var folderName string
-	lastIndex := strings.LastIndex(input, `\`)
-	length := len(input)
-	if lastIndex+1 == length {
-		lastIndex = strings.LastIndex(input[0:length-1], `\`)
-		folderName = input[lastIndex+1 : length-1]
-	} else {
-		folderName = input[lastIndex+1 : length]
+func TestNewImageFromByteArray(t *testing.T) {
+	testImageURL := "https://files.compostintraining.club/media_attachments/files/108/867/955/566/871/361/original/08ec8b2277a3f373.jpeg"
+	resp, err := http.Get(testImageURL)
+	if err != nil {
+		t.Error(err)
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if folderName[len(folderName)-1:] == ":" {
-		return folderName[0 : len(folderName)-1]
-	} else {
-		return folderName
+	config := Generator{
+		DestinationPath:   "",
+		DestinationPrefix: "thumb_",
+		Scaler:            "CatmullRom",
+	}
+	gen := NewGenerator(config)
+	i, err := gen.NewImageFromByteArray(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	teardownTestCase := setupTestCase(t)
+	dest := testDataPath + gen.DestinationPrefix + filepath.Base(testImageURL)
+	defer teardownTestCase(t, dest)
+
+	thumbBytes, err := gen.CreateThumbnail(i)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.WriteFile(dest, thumbBytes, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+
+	checkFileExists(t, dest)
+	var (
+		wantWidth  = gen.Width
+		wantHeight = gen.Height
+	)
+	gotWidth, gotHeight, err := checkImageDimensions(dest)
+	if err != nil {
+		t.Error(err)
+	}
+	if wantWidth != gotWidth {
+		t.Errorf("checkImageDimensions() got %d, wants %d", gotWidth, wantWidth)
+	}
+	if wantHeight != gotHeight {
+		t.Errorf("checkImageDimensions() got %d, wants %d", gotHeight, wantHeight)
+	}
+}
+
+func setupTestCase(t *testing.T) func(t *testing.T, path string) {
+	t.Log("Setting up test case.")
+	return func(t *testing.T, path string) {
+		t.Log("Tearing down test case.")
+		err := os.Remove(path)
+		if err != nil {
+			t.Errorf("Error tearing down test case: %q", err)
+		}
+	}
+}
+
+func checkFileExists(t *testing.T, path string) {
+	_, err := os.Lstat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Error(err)
+			return
+		}
+		t.Errorf("Error running os.Lstat(%q): %q", path, err)
+		return
+	}
+	t.Log("File exists.")
+}
+
+func checkImageDimensions(path string) (int, int, error) {
+	imageBytes, err := os.ReadFile(path)
+	if err != nil {
+		return 0, 0, err
+	}
+	img, _, err := image.Decode(bytes.NewReader(imageBytes))
+	if err != nil {
+		return 0, 0, err
+	}
+	var (
+		width  = img.Bounds().Max.X
+		height = img.Bounds().Max.Y
+	)
+	return width, height, nil
+}
+
+func Example() {
+	var config = Generator{
+		DestinationPath:   "",
+		DestinationPrefix: "thumb_",
+		Scaler:            "CatmullRom",
+	}
+
+	imagePath := "path/to/image.jpg"
+	dest := "path/to/thumb_image.jpg"
+	gen := NewGenerator(config)
+
+	i, err := gen.NewImageFromFile(imagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	thumbBytes, err := gen.CreateThumbnail(i)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(dest, thumbBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (*Generator) ExampleNewImageFromByteArray() {
+	testImageURL := "https://example.com/image.jpg"
+	resp, err := http.Get(testImageURL)
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	config := Generator{
+		DestinationPath:   "",
+		DestinationPrefix: "thumb_",
+		Scaler:            "CatmullRom",
+	}
+	gen := NewGenerator(config)
+	i, err := gen.NewImageFromByteArray(data)
+	if err != nil {
+		panic(err)
+	}
+
+	dest := testDataPath + gen.DestinationPrefix + filepath.Base(testImageURL)
+
+	thumbBytes, err := gen.CreateThumbnail(i)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(dest, thumbBytes, 0644)
+	if err != nil {
+		panic(err)
 	}
 }
